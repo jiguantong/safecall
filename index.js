d58355f9e2ee76ec56ca9c851dc4c6361e1e9288
Guantong <i@gt.email>
2024-01-29 16:03:55 +0800

    exec

--- index.js
+++ index.js
@@ -38,10 +38,21 @@ async function changeCrabOracleOwner() {
     const safeContract = new ethers.Contract("0x00000000079b20FEd6365B38f299D40236A90119", safeABI, _provider);
     const nonce = parseInt(await safeContract.nonce());
     console.log("nonce", nonce);
+    // const owners = await safeContract.getOwners();
+    // console.log("owners", owners);
     const safeTransaction = await safeSdk.createTransaction({ transactions: [safeTransactionData], options: { nonce: nonce } });
     const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
     const senderSignature = await safeSdk.signTransactionHash(safeTxHash);
     console.log(senderSignature, safeTxHash);
+
+    // exec
+    safeTransaction.addSignature(new Safe.EthSafeSignature("0x178E699c9a6bB2Cd624557Fbd85ed219e6faBa77", "0x01e8780e239834cc80ec1e3f52005c197cdc2f5d833aa3bb1fa542ef077d4e301149a6c3037b079172266bf90365ac0db2fd85cb7daaebdf9bf75cdd17c9e2db20"));
+    safeTransaction.addSignature(new Safe.EthSafeSignature("0x9F33a4809aA708d7a399fedBa514e0A0d15EfA85", "0x9fb0f45333311995afabef7977b3b229d57d30a776763b3f96aeeeb4542e35616e51c91ed15543282c2cfeefb4649fcd296728ed7487d0e9eb9d7ed92143e8fe1f"));
+    safeTransaction.addSignature(new Safe.EthSafeSignature("0xA4bE619E8C0E3889f5fA28bb0393A4862Cad35ad", "0x8fe6fdff5478ad107d77c396298450aad89ccc54e6ee4e145ddfc42276524b706f5de1f52067ffad1a9a107ad62004d87850af5d14e30b0fa280daee6eaa3e751f"));
+    
+    const executeTxResponse = await safeSdk.executeTransaction(safeTransaction)
+    const receipt = await executeTxResponse.transactionResponse?.wait()
+    console.log(`txHash: ${receipt.transactionHash}`)
 }
 
 changeCrabOracleOwner();
