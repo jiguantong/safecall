59dcb85a231b428fdf23046d37c084e88da25735
Guantong <i@gt.email>
2024-04-12 21:42:02 +0800

    update reorgs

--- src/checkReorg.js
+++ src/checkReorg.js
@@ -1,21 +1,58 @@
 import { ethers } from 'ethers';
 
-async function getLatestBlockNumber() {
-    const provider = new ethers.JsonRpcProvider("http://c2.collator.itering.io:9944");
-    const blockNumber = await provider.getBlockNumber();
-    return blockNumber;
+const darwiniaRpc = "http://c2.collator.itering.io:9944";
+const moonbeamRpc = "https://moonbeam-rpc.publicnode.com";
+
+async function getBlock(number) {
+    const provider = new ethers.JsonRpcProvider(darwiniaRpc);
+    const block = await provider.getBlock(number || "latest");
+    return block;
 }
 
-let localBlock = 0;
+const range = (start, stop) =>
+    Array.from({ length: stop - start }, (_, i) => start + i);
+
+const isChainConsistent = (
+    blocks,
+) => {
+    for (let i = blocks.length - 1; i > 1; i--) {
+        if (blocks[i].parentHash !== blocks[i - 1].hash) {
+            console.log("!!!###@@@  unexpected hash: ", blocks[i].number, blocks[i].parentHash, blocks[i - 1].number, blocks[i - 1].hash);
+            return false
+        };
+    }
+    return true;
+};
+
+let localBlocks = [];
 
 async function main() {
     setInterval(async () => {
-        const newBlock = await getLatestBlockNumber();
-        if (newBlock >= localBlock) {
-            localBlock = newBlock;
-            // console.log("local <= new", localBlock, newBlock);
-        } else {
-            console.error(`!!! local(${localBlock}) > new(${newBlock}) diff: ${localBlock-newBlock}`);
+        const newBlock = await getBlock();
+        if (localBlocks.length == 0) {
+            localBlocks.push(newBlock);
+            return;
+        }
+        const lastLocalBlock = localBlocks[localBlocks.length - 1];
+        if (newBlock.number == lastLocalBlock.number) {
+            // console.log("skip == ", newBlock.number);
+        } else if (newBlock.number < lastLocalBlock.number) {
+            // reorg
+            console.log(`${new Date().getTime()}: !!reorg1 newBlock: ${newBlock.number} < localBlock: ${lastLocalBlock.number}`)
+        } else if (newBlock.number > lastLocalBlock.number) {
+            // normal
+            const missingRange = range(lastLocalBlock.number + 1, newBlock.number);
+            // console.log("missingRange: ", missingRange);
+            const newBlocks = await Promise.all(
+                missingRange.map(getBlock)
+            )
+            newBlocks.push(newBlock);
+            if (!isChainConsistent([...localBlocks, ...newBlocks])) {
+                console.log(`${new Date().getTime()}: !!!### reorg !consistent ${newBlock.number}`);
+            } else {
+                localBlocks.push(...newBlocks);
+                console.log(`${new Date().getTime()}: append blocks`, lastLocalBlock.number, newBlocks[newBlocks.length - 1].number, localBlocks.length);
+            }
         }
     }, 1000)
 }
